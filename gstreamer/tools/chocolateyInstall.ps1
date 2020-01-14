d186c9dac3c60d33eda3f6b983cf78b96f154c38
Jack McKernan <jmcker@outlook.com>
2020-01-14 10:23:19 -0500

    Add setup for 32-bit installer

--- gstreamer/tools/chocolateyInstall.ps1
+++ gstreamer/tools/chocolateyInstall.ps1
@@ -9,16 +9,20 @@ $version        = "${major}.${minor}.${revision}"
 
 $packageName    = "${ENV:ChocolateyPackageName}"
 $packageTitle   = "${ENV:ChocolateyPackageTitle}"
+$url            = "https://gstreamer.freedesktop.org/data/pkg/windows/${version}/gstreamer-1.0-msvc-x86-${version}.msi"
 $url64          = "https://gstreamer.freedesktop.org/data/pkg/windows/${version}/gstreamer-1.0-msvc-x86_64-${version}.msi"
 $silentArgs     = "/qn /norestart /l*v `"$(${ENV:TEMP})\$(${packageName}).$(${version}).MsiInstall.log`""
 
 $packageArgs = @{
   packageName     = "${packageName}"
   softwareName    = "${packageTitle}"
+  # url             = "${url}"
   url64           = "${url64}"
   fileType        = "msi"
   silentArgs      = "${silentArgs}"
   validExitCodes  = @(0)
+  # checksum        = ""
+  # checksumType    = "sha256"
   checksum64      = "F33FFF17A558A433B9C4CF7BD9A338A3D0867FA2D5EE1EE33D249B6A55E8A297"
   checksumType64  = "sha256"
 }
