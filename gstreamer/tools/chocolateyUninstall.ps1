c7c28a7d68a82092045fc87906112a33061f129f
Jack McKernan <jmcker@outlook.com>
2020-05-27 08:51:58 -0500

    Add empty uninstall script to GStreamer

---
+++ gstreamer/tools/chocolateyUninstall.ps1
@@ -0,0 +1,7 @@
+$ErrorActionPreference = 'Stop';
+
+# Let the auto-uninstaller take care of things
+
+# Don't mess with PATH for now since there is currently no easy way to remove
+# our entry without possibly mangling everything
+# https://github.com/chocolatey/choco/issues/310
