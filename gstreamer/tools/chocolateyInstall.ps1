6dff4a1ae842fbbe43942865a2ad421b00b3abd4
Jack McKernan <jmcker@outlook.com>
2020-09-09 15:47:32 -0400

    Updates for GStreamer 1.18.0
    Added msvc subfolder to download url
    Updated PATH to use the renamed GSTREAMER_ROOT* environment variables

--- gstreamer/tools/chocolateyInstall.ps1
+++ gstreamer/tools/chocolateyInstall.ps1
@@ -9,21 +9,21 @@ $version        = "${major}.${minor}.${revision}"
 
 $packageName    = "${ENV:ChocolateyPackageName}"
 $packageTitle   = "${ENV:ChocolateyPackageTitle}"
-$url            = "https://gstreamer.freedesktop.org/data/pkg/windows/${version}/gstreamer-1.0-msvc-x86-${version}.msi"
-$url64          = "https://gstreamer.freedesktop.org/data/pkg/windows/${version}/gstreamer-1.0-msvc-x86_64-${version}.msi"
+$url            = "https://gstreamer.freedesktop.org/data/pkg/windows/${version}/msvc/gstreamer-1.0-msvc-x86-${version}.msi"
+$url64          = "https://gstreamer.freedesktop.org/data/pkg/windows/${version}/msvc/gstreamer-1.0-msvc-x86_64-${version}.msi"
 $silentArgs     = "ADDLOCAL=ALL /qn /norestart /l*v `"$(${ENV:TEMP})\$(${packageName}).$(${version}).MsiInstall.log`""
 
 $packageArgs = @{
   packageName     = "${packageName}"
   softwareName    = "${packageTitle}"
-  # url             = "${url}"
+  url             = "${url}"
   url64           = "${url64}"
   fileType        = "msi"
   silentArgs      = "${silentArgs}"
   validExitCodes  = @(0)
-  # checksum        = ""
-  # checksumType    = "sha256"
-  checksum64      = "F33FFF17A558A433B9C4CF7BD9A338A3D0867FA2D5EE1EE33D249B6A55E8A297"
+  checksum        = "1EB3F35A799BDDAC561A252F0EDA763DD6546C04B2D6B0D6FE6D4803C15CEAD6"
+  checksumType    = "sha256"
+  checksum64      = "E766A67C254EFBC090B58DEC6B141832DF0BA8CAB1C56F9CB05BF5A4601758AC"
   checksumType64  = "sha256"
 }
 
@@ -33,7 +33,7 @@ Write-Output ""
 
 # Must install to User path since we need to expand a User environment variable
 if (${ENV:OS_IS64BIT} -And -Not ${ENV:ChocolateyForceX86}) {
-  Install-ChocolateyPath -PathToInstall "%GSTREAMER_1_0_ROOT_X86_64%\bin" -PathType "User"
+  Install-ChocolateyPath -PathToInstall "%GSTREAMER_1_0_ROOT_MSVC_X86_64%\bin" -PathType "User"
 } else {
-  Install-ChocolateyPath -PathToInstall "%GSTREAMER_1_0_ROOT_X86%\bin" -PathType "User"
+  Install-ChocolateyPath -PathToInstall "%GSTREAMER_1_0_ROOT_MSVC_X86%\bin" -PathType "User"
 }
